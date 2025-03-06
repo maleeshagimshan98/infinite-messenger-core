7b7c5e3fe3cb24a2e0b7640d921e6c54d6a3a64e
Maleesha Gimshan <maleeshagimshan74@gmail.comn>
2025-03-06 23:36:42 +0530

    Created DatabaseResult utility class

---
+++ src/datastore/utils/DatabaseResult.ts
@@ -0,0 +1,45 @@
+/**
+ * Copyright - 2025 - Maleesha Gimshan (github.com/maleeshagimshan98)
+ */
+
+class DatabaseResult {
+
+  /**
+   * Data object.
+   * 
+   * @type {Record<string, any>}
+   */
+  private _data: Record<string, any> = {};
+
+  /**
+   * Indicates if data is present.
+   * 
+   * @type {boolean}
+   */
+  private _hasData: boolean = false;
+
+  constructor(data: Record<string, any> | undefined) {
+    this._data = data ?? {};
+    this._hasData = data ? Object.keys(data).length > 0 : false;
+  }
+
+  /**
+   * Returns the _data object.
+   *
+   * @returns {Record<string, any>} The current data.
+   */
+  data(): Record<string, any> {
+    return this._data;
+  }
+
+  /**
+   * Returns the boolean value of _hasData.
+   *
+   * @returns {boolean} True if data is present, otherwise false.
+   */
+  hasData(): boolean {
+    return this._hasData;
+  }
+}
+
+export default DatabaseResult;
